cea32114bdd848c345e98a2cb8f71025b43a7c50
Joachim Kohlhammer <joachim.kohlhammer@gmx.de>
2024-02-04 18:20:10 +0100

    Calculating the histogram only once per image

--- pyfixes/images/removeDuplicateImages.py
+++ pyfixes/images/removeDuplicateImages.py
@@ -1,28 +1,39 @@
 import cv2
 import os
+import time
 
 
-def compareHistogram(if1, if2):
-    image1 = cv2.imread(if1)
-    image2 = cv2.imread(if2)
-    histImg1 = cv2.calcHist([image1], [0, 1, 2], None, [256, 256, 256], [0, 256, 0, 256, 0, 256])
-    histImg1[255, 255, 255] = 0
-    cv2.normalize(histImg1, histImg1, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX)
-    histImg2 = cv2.calcHist([image2], [0, 1, 2], None, [256, 256, 256], [0, 256, 0, 256, 0, 256])
-    histImg2[255, 255, 255] = 0
-    cv2.normalize(histImg2, histImg2, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX)
-    return cv2.compareHist(histImg1, histImg2, cv2.HISTCMP_CORREL)
+# Similarity is between -1..1 (different..similar)
+THRESHOLD = 0.35
 
 
+def isDuplicate(histImg1, histImg2):
+    score = cv2.compareHist(histImg1, histImg2, cv2.HISTCMP_CORREL)
+    return score > THRESHOLD
+
+
+def getHistogram(imageFile):
+    image = cv2.imread(imageFile)
+    histImg = cv2.calcHist([image], [0, 1, 2], None, [256, 256, 256], [0, 256, 0, 256, 0, 256])
+    histImg[255, 255, 255] = 0
+    cv2.normalize(histImg, histImg, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX)
+    return histImg
+
+
+start = time.time()
 newImageTag = []
 keepImages = []
 removeImages = []
+histograms = dict()
 
 home = GC.athlete()['home']
 mediaDir = home + os.sep + 'media' + os.sep
 m = GC.activityMetrics()
-print(m['Images'].split())
 allFiles = [mediaDir + image for image in m['Images'].split() if os.path.isfile(mediaDir + image)]
+
+for file in allFiles:
+    if file not in histograms.keys():
+        histograms[file] = getHistogram(file)
 
 while len(allFiles) > 0:
     currentFile = allFiles.pop()
@@ -30,15 +41,14 @@ while len(allFiles) > 0:
         continue
     keepImages.append(currentFile)
     newImageTag.append(os.path.basename(currentFile))
-    print("--- Next file: " + os.path.basename(currentFile))
 
     for compareFile in allFiles:
         if compareFile in removeImages or compareFile in keepImages:
             continue
-        histogramScore = compareHistogram(currentFile, compareFile)
-        print("Similarities: {} <-> {} histogram = {}".format(os.path.basename(currentFile), os.path.basename(compareFile), round(histogramScore, 2)))
-        if histogramScore >= 0.2:
+        if isDuplicate(histograms.get(currentFile), histograms.get(compareFile)):
             removeImages.append(compareFile)
+
+print("Keeping " + ", ".join(newImageTag))
 
 for image in removeImages:
     print("Removing " + os.path.basename(image))
@@ -46,3 +56,5 @@ for image in removeImages:
 
 if len(removeImages) > 0:
     GC.setTag('Images', '\n'.join(newImageTag))
+end = time.time()
+print("Finished in {:.2f} seconds".format(end - start))
